08a1f6d14606b55a661fef5f088c6cf975bbfbde
Oisín_windows <byrne.ois@gmail.com>
2021-04-27 11:18:34 +0100

    json test

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import logo from './imgs/the404.png';
 
 const Header = () => {
@@ -8,7 +9,9 @@ const Header = () => {
     return ( 
         <header className="header-container">
             <div className='logo-container'>
-                <img src={logo} alt="the404" className="logo"/>
+                <Link to='/yo'>
+                    <img src={logo} alt="the404" className="logo"/>
+                </Link>
             </div>
         </header>
      );
