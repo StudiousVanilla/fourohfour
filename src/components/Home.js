e494ec5006a6a9d84b82ae1d3ad4542b168a96da
Oisín_windows <byrne.ois@gmail.com>
2021-04-27 11:02:26 +0100

    mobile video test

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,26 +3,29 @@
 const Home = () => {
     return ( 
         <div className="home-container">
+                <div className="video-container">
+                    <iframe title="chessVid" src="https://player.vimeo.com/video/512623670?title=0&byline=0&portrait=0" frameborder="0" allowfullscreen></iframe>
+                </div>
+                <div className="video-container">
+                    <iframe title="tradingVid" src="https://player.vimeo.com/video/507626329?title=0&byline=0&portrait=0" frameborder="0" allowfullscreen></iframe>
+                </div>
+                <div className="video-container">
+                    <iframe title="trustVid" src="https://player.vimeo.com/video/444053665?title=0&byline=0&portrait=0" frameborder="0" allowfullscreen></iframe>
+                </div>
+                <div className="video-container">
+                    <iframe src="https://player.vimeo.com/video/512623670?title=0&byline=0&portrait=0" frameborder="0" allowfullscreen></iframe>
+                </div>
+                <div className="video-container">
+                    <iframe src="https://player.vimeo.com/video/512623670?title=0&byline=0&portrait=0" frameborder="0" allowfullscreen></iframe>
+                </div>
+                <div className="video-container">
+                    <iframe src="https://player.vimeo.com/video/512623670?title=0&byline=0&portrait=0" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen  className="video"></iframe>
+                </div>
+
             <div className="video-container">
-                <iframe title="nftVid" src="https://player.vimeo.com/video/521538066"   frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen className="video"></iframe>
-                <p className="video-title"><a href="https://vimeo.com/521538066">Block Party: NFTs and the environment</a></p>
+                <iframe title="stormVid" src="https://player.vimeo.com/video/453370010?title=0&byline=0&portrait=0" frameBorder="0"  allowFullScreen className="video"></iframe>
             </div>
-            <div className="video-container">
-                <iframe title="chessVid" src="https://player.vimeo.com/video/512623670" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen className="video"></iframe>
-                <p className="video-title"><a href="https://vimeo.com/512623670">Gambits in the Chat: Chess Streaming</a></p>
-            </div>
-            <div className="video-container">
-                <iframe title="tradingVid" src="https://player.vimeo.com/video/507626329"frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen className="video"></iframe>
-                <p className="video-title"><a href="https://vimeo.com/507626329">The Reddit Wall Street Bets Trading Frenzy</a></p>
-            </div>
-            <div className="video-container">
-                <iframe title="trustVid" src="https://player.vimeo.com/video/444053665" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen className="video"></iframe>
-                <p className="video-title"><a href="https://vimeo.com/444053665">Antitrust Issue: Big Tech Testimony</a></p>
-            </div>
-            <div className="video-container">
-                <iframe title="stormVid" src="https://player.vimeo.com/video/453370010" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen className="video"></iframe>
-                <p className="video-title"><a href="https://vimeo.com/453370010">#6 Virtual Storm Chasers</a></p>
-            </div>
+            {/*  
             <div className="video-container">
                 <iframe title="microsoftVid" src="https://player.vimeo.com/video/446161639" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen className="video"></iframe>
                 <p className="video-title"><a href="https://vimeo.com/446161639">#3 Microsoft on the Clock</a></p>
@@ -30,7 +33,7 @@ const Home = () => {
             <div className="video-container">
                 <iframe title="dataVid" src="https://player.vimeo.com/video/441810952" frameBorder="0" allow="autoplay; fullscreen; picture-in-picture" allowFullScreen className="video"></iframe>
                 <p className="video-title"><a href="https://vimeo.com/441810952">Fishing for Likes: Digging into CrowdTangle&#039;s Daily Data</a></p>
-            </div>
+            </div> */}
         </div>
      );
 }
